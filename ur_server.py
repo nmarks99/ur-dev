6824300eb27272a81f20011c41cacb9275da6475
Nick Marks <nmarks@anl.gov>
2023-12-06 12:56:21 -0600

    update ur_server.py to what I tested at 8idg

--- ur_server.py
+++ ur_server.py
@@ -1,39 +1,36 @@
+#!/usr/bin/env python3
 import socket
+from epics import caget
 
-HOST = "164.54.104.7"
-PORT = 31001
+HOST = "164.54.116.80" # pepper IP address
+PORT = 31111
+BUFFER_SIZE = 1024
+PREFIX = "8idgur3e"
 count = 0
-
-var_dict = {
-    'BlockID' : 999,
-}
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.bind((HOST, PORT))
     sock.listen()
     while True:
         try: 
-            print("Waiting for connection...")
+            # print("Waiting for connection...")
             connection, address = sock.accept()
             with connection:
-                print(f"[{count}] Connection opened on {address}")
+                # print(f"[{count}] Connection opened on {address}")
                 while True:
                     data = connection.recv(1024)
                     if not data:
                         break
                     data_recv = data.decode("ascii")
-                    print(f"Recieved: {repr(data_recv)}")
+                    # print(f"Recieved: {repr(data_recv)}")
                    
-                    var = data_recv.split(" ")[1].strip() # fails if bad received data
-                    if var in var_dict.keys():
-                        value = var_dict[var]
-                        reply = f"{var} {value}\n"
-                        print(f"To send: {repr(reply)}")
-                        connection.sendall(reply.encode("utf-8"))
-                    else:
-                        print(f"variable {var} not found")
-                        connection.sendall("1".encode("utf-8"))
-            print(f"Connection to client closed\n")
+                    var = data_recv.split(" ")[1].strip()
+                    value = caget(f"{PREFIX}:{var}.VAL")
+                    reply = f"{var} {value}\n"
+                    # print(f"To send: {repr(reply)}")
+                    connection.sendall(reply.encode("utf-8"))
+
+            # print(f"Connection to client closed\n")
             count += 1
         except KeyboardInterrupt:
             print("\nServer quit by user")
